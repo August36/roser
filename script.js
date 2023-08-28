5e4a2ae93e0393fdc918fe53a3d09d1ce4cd0b43
August <augustjp@live.dk>
2023-08-28 11:07:16 +0200

    pluk_roser_opgave_28_08_23

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // skriv din kode her.
-
+const roser = document.querySelectorAll("main img")
 // vælg alle blomster og add en click eventlistener til dem.
-
+roser.forEach((rose) => rose.addEventListener("click", plukRose));
 // lav en funktion der tilføjer class'en "pluk" til den blomst der bliver klikket på.
+function plukRose(){
+    this.classList.toggle("pluk");
+}
